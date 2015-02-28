d45a9d8354356fcf18563e9e5231ec8fb7955539
Hermogenes Batista da Silva Filho <exemogenes@gmail.com>
2015-02-28 16:02:19 -0300

    Adicionado a criação do banco e tabela(keyspace).

--- exemples/createKeyspace.py
+++ exemples/createKeyspace.py
@@ -5,8 +5,18 @@ from models.Model import ConectCassandra
 __author__ = 'Administrador'
 
 if __name__ == "__main__":
-    cluster = ConectCassandra('test')
+    bd = 'test'
+    cluster = ConectCassandra(bd)
     cluster.session.execute('''CREATE KEYSPACE IF NOT EXISTS '''+cluster.keyspace+'''
     WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 3};''')
+    cluster.conect()
 
+    cluster.session.execute('''CREATE TABLE IF NOT EXISTS users(
+                            id uuid,
+                            date timestamp,
+                            user_name varchar,
+                            gender varchar,
+                            birth_year bigint,
+                            PRIMARY KEY(id, date));
+                        ''')
 
